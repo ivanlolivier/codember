8167c48b7640b52e4af58d9899b56084247d3907
Ivan L'olivier <ivan.lolivier@gmail.com>
2022-11-09 03:00:09 -0300

    Using fromEntries function

--- challenge1/index.js
+++ challenge1/index.js
@@ -7,14 +7,7 @@ const data = fs.readFileSync('users.txt', 'utf8');
 const users = data
   .split('\n\n')
   .map((line) => line.replaceAll('\n', ' ').split(/\s+/))
-  .map(userArr => {
-    const userObj = {};
-    for (const userItem of userArr) {
-      const [key, value] = userItem.split(':');
-      userObj[key] = value;
-    }
-    return userObj;
-  });
+  .map(userArr => Object.fromEntries(userArr.map(u => u.split(':'))));
 
 
 const validUsers = users.filter(user => REQUIRED_FIELD_NAMES.every(fieldName => fieldName in user));

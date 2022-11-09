18b347741460edca8929d8dfe64e60fa80e3d5ed
Ivan L'olivier <ivan.lolivier@gmail.com>
2022-11-09 03:05:24 -0300

    Adding some comments

--- challenge1/index.js
+++ challenge1/index.js
@@ -5,8 +5,11 @@ const REQUIRED_FIELD_NAMES = ['usr', 'eme', 'psw', 'age', 'loc', 'fll'];
 const data = fs.readFileSync('users.txt', 'utf8');
 
 const users = data
+  //create an array with 1 item per user (split on double newline)
   .split('\n\n')
-  .map((line) => line.replaceAll('\n', ' ').split(/\s+/))
+  // getting rid of newlines inside a user and splitting by spaces+
+  .map((rawUser) => rawUser.replaceAll('\n', ' ').split(/\s+/))
+  // splitting each user part (ie: age:21) into key/value pairs and creating an object
   .map(userArr => Object.fromEntries(userArr.map(u => u.split(':'))));
 
 const validUsers = users.filter(user => REQUIRED_FIELD_NAMES.every(fieldName => fieldName in user));
